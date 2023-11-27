33bd750d02a90939799113f9e612baab028981b5
GitHubAlejandroDR <alejandrodominguezrecio@uma.es>
2023-11-27 10:49:25 +0100

    Loading data folders structure, config standard paths

--- code/configure.R
+++ code/configure.R
@@ -23,6 +23,13 @@ DATA_PROCESSED_DIR <- r"(C:/Users/aleja/Documents/github_repositories/prognosis-
 RESULT_DIR <- r"(C:/Users/aleja/Documents/github_repositories/prognosis-prediction-TCGA/results/)"
 PLOTS_DIR <- r"(C:\Users\aleja\Documents\github_repositories\prognosis-prediction-TCGA\plots\)"
 
+# CODE_DIR = r"(/path_to_your_directory/prognosis-prediction-TCGA/code/)"
+# RMDs_DIR = r"(/path_to_your_directory/prognosis-prediction-TCGA/RMDs/)"
+# DATA_DIR = r"(/path_to_your_directory/prognosis-prediction-TCGA/data/raw/)"
+# DATA_PROCESSED_DIR <- r"(/path_to_your_directory/prognosis-prediction-TCGA/data/processed/)"
+# RESULT_DIR <- r"(/path_to_your_directory/prognosis-prediction-TCGA/results/)"
+# PLOTS_DIR <- r"(/path_to_your_directory/prognosis-prediction-TCGA/plots/)"
+
 # %%%%%%%%%%%%%%
 # RAW DATA PATHS
 
@@ -31,6 +38,12 @@ RAW_CLINICAL_SAMPLE <- r"(C:/Users/aleja/Documents/github_repositories/prognosis
 RAW_GENOMIC <- r"(C:/Users/aleja/Documents/github_repositories/prognosis-prediction-TCGA/data/raw/data_mrna_seq_v2_rsem.txt)"
 RAW_METHYLATION <- r"(C:/Users/aleja/Documents/github_repositories/prognosis-prediction-TCGA/data/raw/data_methylation.txt)"
 RAW_MICROBIOME <- r"(C:/Users/aleja/Documents/github_repositories/prognosis-prediction-TCGA/data/raw/data_microbiome.txt)"
+
+# RAW_CLINICAL_PATIENT <- r"(/path_to_your_directory/prognosis-prediction-TCGA/data/raw/data_clinical_patient.txt)"
+# RAW_CLINICAL_SAMPLE <- r"(/path_to_your_directory/prognosis-prediction-TCGA/data/raw/data_clinical_sample.txt)"
+# RAW_GENOMIC <- r"(/path_to_your_directory/prognosis-prediction-TCGA/data/raw/data_mrna_seq_v2_rsem.txt)"
+# RAW_METHYLATION <- r"(/path_to_your_directory/prognosis-prediction-TCGA/data/raw/data_methylation.txt)"
+# RAW_MICROBIOME <- r"(/path_to_your_directory/prognosis-prediction-TCGA/data/raw/data_microbiome.txt)"
 
 # %%%%%%%%%%%%%%%%%%%%%
 # STATISTICAL CONSTANTS
